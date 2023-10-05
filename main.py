e917b1ed1d186966a8aae91c0309c84c55eb9d3a
Adam <ks20447@bristol.ac.uk>
2023-10-05 10:10:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+"""Final Year Technical Project - Anti-Aligning Robots
+
+The control of macroscopic robot collectives is an important techniques in many technological
+applications. At a fundamental level, it is important to understand how the rules governing the robot
+interactions influence the emergent collective behavior of a robot swarm. In particular, seemingly
+innocuous changes in the interaction rules can lead to drastically different collective dynamics. This
+project will design and develop a robot swarm whose agents preferentially anti-align (in contrast to
+classical models of aligning particles such as the Viczek model of collective motion), comparing the
+results to theoretical models developed by collaborators at Brandeis University.
+
+Author - Adam Morris
+Created - 05/10/2023
+
+"""
+
